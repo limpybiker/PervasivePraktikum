5573a990d4c8edab1639c64118a515179f2dd6c5
limpybiker <limpybiker@hotmail.com>
2011-11-18 01:37:35 +0100

    python udp client zum testen

---
+++ udpClient.py
@@ -0,0 +1,25 @@
+﻿# Client program
+
+from socket import *
+
+# Set the socket parameters
+host = "localhost"
+port = 1234
+buf = 1024
+addr = (host,port)
+
+# Create socket and bind to address
+UDPSock = socket(AF_INET,SOCK_DGRAM)
+UDPSock.bind(addr)
+
+# Receive messages
+while 1:
+	data,addr = UDPSock.recvfrom(buf)
+	if not data:
+		print "Client has exited!"
+		break
+	else:
+		print data
+
+# Close socket
+UDPSock.close()
